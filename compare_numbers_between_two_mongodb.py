6b3963acd724174e1befdee564ebd1190af1d2c1
thorsivodin <thorsivodin@gmail.com>
2019-12-18 00:56:38 -0800

    mongo compare python script

---
+++ compare_numbers_between_two_mongodb.py
@@ -0,0 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+#why there is a 'i=0' ? .....lazy
+i = 0
+while i < 1:
+        #connect to two mongodb instances
+        conn1 = MongoClient('mongodb://122.226.73.41:61111')
+        conn2 = MongoClient('mongodb://127.0.0.1:27917')
+        #get datalist from source mongodb
+        dblist = conn1.list_database_names()
+        #compare numbers of every collection between mongodb instances
+        for db in dblist:
+                collist = conn1[db].list_collection_names()
+                for coll in collist:
+                        numc1 = conn1[db][coll].find().count()
+                        numc2 = conn2[db][coll].find().count()
+                        if numc1 != numc2:
+                                print "db: %20s   |   collection: %40s   |   num1: %20d   |   num2: %20d" % (db,coll,numc1,numc2)
+
+        i+=1
